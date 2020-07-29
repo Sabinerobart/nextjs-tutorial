3b646798cff5141d8ecfd361d7da33e4aa348363
Sabine Robart <sabine.robart@protonmail.com>
2020-07-29 15:11:17 +0200

    Fetch static data (includes fetching server-side data how to)

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,33 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+/**
+ * getStaticProps for static generation (the page doesn't change on every request)
+ */
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+/**
+ * getServerSideProps for server-side rendering (frequently updated by user requests)
+ * - Check documentation to fill the 'context' object
+ */
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     }
+//   }
+// }
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -20,6 +45,33 @@ export default function Home() {
           </p>
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
 }
+
+/**
+ * React hook for data fetching
+ */
+// import useSWR from 'swr'
+
+// function Profile() {
+//   const { data, error } = useSWR('/api/user', fetch)
+
+//   if (error) return <div>failed to load</div>
+//   if (!data) return <div>loading...</div>
+//   return <div>hello {data.name}!</div>
+// }
